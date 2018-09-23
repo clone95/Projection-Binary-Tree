d6347adefe7596203ac1cc09f6af94b8b4f1d60d
jack <giac290595@gmail.com>
2018-09-23 15:39:39 +0100

    added some minor features

--- binary_tree.py
+++ binary_tree.py
@@ -2,28 +2,31 @@ from anytree import Node, RenderTree
 
 
 def increment(to_incr, n):     #increments of n
-    val = to_incr + (to_incr*n)
-    return val
+    return to_incr*n
 
 
 def decrement(to_decr, n):     #increments of n
-    val = to_decr - (to_decr*n)
-    return val
+    return to_decr*n
 
 
-def generate_level(node, n):
-        nod1 = Node("salita", node)
-        nod2 = Node("discesa", node)
+def generate_level(node, n, m):
+        m = m + 1
+        nod1 = Node(root.name, node)
+        nod2 = Node(root.name, node)
+        #nod1 = Node(root.name + increment(root.name, 0.1)*m, node)
+        #nod2 = Node(root.name - increment(root.name, 0.1)*m, node)
+        nod1.name = nod1.parent.name + 1
+        nod2.name = nod2.parent.name - 1
         if n != 0:
-            generate_level(nod1, n-1)
-            generate_level(nod2, n-1)
+            generate_level(nod1, n-1, m)
+            generate_level(nod2, n-1, m)
 
 
 leaves = 2
 branches = 3
 root = Node(100)
 
-generate_level(root, 3)
+generate_level(root, 3, 0)
 
 print(RenderTree(root))
 
